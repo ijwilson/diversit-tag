159cda56b6a38bcac9e29bf9e2c07ef5c04eef94
Ian Wilson <wilson.ianj@gmail.com>
2016-03-08 14:40:11 +0000

    testng equations further

--- R/functions.R
+++ R/functions.R
@@ -11,99 +11,6 @@ if (!require("stringdist")) {
   install.packages("stringdist")
   require(stringdist)
 }
-## recursively merge a set of reads that are indexed by tag
-mergeall <- function(mm) {
-  merge1 <- function(ll) {
-    n <- length(ll)
-    res <- list()
-    for (ii in seq(2,n,2)) {
-      res[[ii%/%2]] <- merge(ll[[ii-1]], ll[[ii]], all=TRUE)
-    }  
-    if (length(ll)%%2==1) {
-      res[[length(res)+1]] <- ll[[n]]
-    }
-    res
-  }
-  while(length(mm)>1) {
-    mm <- merge1(mm)
-  }
-  mm[[1]]
-}
-
-
-
-setup <- function(experimentnames) {
-  l <- lapply(experimentnames,
-          function(x){
-            load(paste("rda/",x,".rda",sep=""));
-            get(x);
-          }
-  )
-  m <- mergeall(l)
-  m[is.na(m)] <- 0
-  m
-}
-
-#############################################################################################
-rawestimatep <- function(p, p_baseline) {
-  mean((p-p_baseline)^2)/mean(p_baseline*(1-p_baseline))
-} 
-#############################################################################################
-D2 <- function(p1, p2) {
-##  http://evolution.genetics.washington.edu/phylip/doc/gendist.html
-  (sum((p1-p2)^2))/(sqrt(sum(p1^2))*sqrt(sum(p2^2)))
-}
-
-FF <- function(p1, p2) {
-  (sum((p1-p2)^2))/(2*(1-sum(p1*p2)))
-} 
-
-
-estimateD2 <- function(count1, count2, min_count=0) {
-  u <- count1 >= min_count | count2 > min_count
-  p1 <- count1[u]/sum(count1[u])
-  p2 <- count2[u]/sum(count2[u])
-  
-  D2(p1, p2)
-}
-
-
-estimateFF <- function(count1, count2, min_count=0) {
-  u <- count1 >= min_count | count2 > min_count
-  p1 <- count1[u]/sum(count1[u])
-  p2 <- count2[u]/sum(count2[u])
-  
-  FF(p1, p2)
-}
-#############################
-estimatecount <- function(count2, count_baseline, min_baseline=0, min_p=0) {
-  pbase <- count_baseline/sum(count_baseline)
-  u <- count_baseline>=min_baseline & pbase >= min_p
-  p <- count2[u]/sum(count2[u])
-  p_b <- count_baseline[u]/sum(count_baseline[u])
-  rawestimatep(p, p_b)
-}
-#############################
-estimatecount_equalfreq <- function(count2) {
-  p <- 1/length(count2)
-  p_b <- count2/sum(count2)
-  mean((p_b-p)^2)*length(count2)
-}
-#############################
-estimatecount2 <- function(count2, count_baseline, min_baseline=0, min_p=0) {
-  pbase <- count_baseline/sum(count_baseline)
-  u <- count_baseline>=min_baseline & pbase >= min_p
-  p <- count2[u]/sum(count2[u])
-  p_b <- count_baseline[u]/sum(count_baseline[u])
-  mean((p-p_b)^2)/mean(p_b*(1-p_b)) - 1/sum(count2) - 1/sum(count_baseline)
-}
-if (!exists("dummy")) {
-## Code run to get the experiment names
-experiments <- scan("all.files", sep="\n",what=character(0))
-experiments <- gsub(pattern = "Mouse ","",experiments)
-experiments <- gsub(pattern = ".xlsx","",experiments)
-ex <- experiments
-}
 
 merge_by_tag <- function(d1, d2) {
   da <- merge(d1, d2, by="tag", all=TRUE)
@@ -153,11 +60,22 @@ tagdistplot <- function(tags, f, howmany=5) {
   require(ggplot2)
   u <- d>0 & f>0
   
-  pl <- ggplot(data.frame(x=d[u], y=f[u], closest=factor(tag1closest[u])),aes(x=x,y=y, col=closest)) + geom_jitter(width=0.05, alpha=0.8) + scale_y_log10("Frequency of Tag") 
+  pl <- ggplot(data.frame(x=d[u], y=f[u], closest=factor(tag1closest[u])),aes(x=x,y=y, col=closest)) 
+  pl <- pl + geom_jitter(width=0.05, alpha=0.8) + scale_y_log10("Frequency of Tag") 
   pl <- pl + scale_x_continuous("Distance from Most Frequent Tag", breaks=c(1,4,8,12))
-pl + scale_colour_brewer(palette="Dark2")
+  pl + scale_colour_brewer(palette="Dark2")
 }
-#############################
-estimatecount_equalfreq <- function(count2) {
-
+##################################################
+simulate_drifttags <- function(n_tags, total_depth, drift_G, alpha=0.02) {
+  rdirichlet <- function(k, beta) {
+    a <- rgamma(k, beta, 1.0)
+    a/sum(a)
+  }
+  background_p <- rdirichlet(n_tags, alpha)
+  background_count <- rpois(n_tags, total_depth*background_p)
+  driftdistribution <- background_p*(1/drift_G-1)
+  p <- rdirichlet(n_tags, driftdistribution)
+  count <- rpois(n_tags, total_depth*p)
+  data.frame(count=count, background_count=background_count, p=p, background_p = background_p)
 }
+##################################################
--- README.md
+++ README.md
@@ -28,8 +28,8 @@ AAATCGCGATGCAAAGCGGTTTAACCC|   1213  |  365   |    400
 These can be read into R in the usual way
 
 ````
-ex1 <- read.table("data/example1.txt", header=TRUE)
-ex2 <- read.table("data/example2.txt", header=TRUE)
+ex1 <- read.table("data/example1.txt", header=TRUE, stringsAsFactors=FALSE)
+ex2 <- read.table("data/example2.txt", header=TRUE, stringsAsFactors=FALSE)
 ```
 
 ### Combining data sets
@@ -38,7 +38,7 @@ The R function `merge_by_tag` allows you to merge data sets
 together.  Tags that are not present in either of the data sets 
 will be given a zero count.
 
-```
+```{r}
 source("R/functions.R")
 ex <- merge_by_tag(ex1, ex2)
 ```
@@ -88,34 +88,81 @@ Simple R functions have been added to simplify the analysis of tag sharing.
 If tags are coded as DNA then the distances between tags can be simply measured by using the `stringdist` library.
 
 ```{r}
+cat(ex$tag[1], ex$tag[2], sep="\n")
 stringdist(ex$tag[1], ex$tag[2])
 ```
 
 The function `tagdistplot` allows you to get plot the frequencies of 
-all the tags vs. the distance from the most frequent tag (in addition those tags that are 1 step away from the 2nd, 3rd, ... , `howmany`) tags are colour coded.  This allows you to see how 1-step errors are a major 
+all the tags vs. the distance from the most frequent tag (in addition those 
+tags that are 1 step away from the 2nd, 3rd, ... , `howmany`) tags are colour 
+coded.  This allows you to see how 1-step errors are a major 
 reason for low frequency variants in some runs.
 
 ```
 tagdistplot(ex$tag, freq$Pop1_A, howmany=4)
 ```
 
+![plot of closest tags](fig/closest.png)
 
-### Estimating Nei's G
+We can see how there is a large cluster of tags that are a single step 
+away from the most frequent tag.
 
+## Estimating Nei's G
 
-$$ 
-a = \Sum_{i=1} c_i
+### Theory
+
+#### Drift from equal frequencies
+
+In the case of the first transplant a number of tags are selected from 
+a pool with equal frequencies and then we observe the number of counts 
+after sequencing.  
+
+#### Drift from baseline
+
+We assume that the counts we see are a representation of an underlying
+tag frequency distribution that is observed with error (we assume that 
+the counts are poisson distributed with mean equal to $n*p_i$ where $n$
+is the total coverage and $p_i$ is the frequency of tag $i$) then, we can 
+estimate $G$ by combining over all alleles
+
+$$
+\hat{G} = \frac{\sum_i (p_i-b_i)^2}{\sum_i b_i(1-b_i)}
 $$
 
-We estimate Nei's G using the function `estimate_G`.  This takes a 
-pair of parameters, the first is the count, the second, if present gives 
-the frequency of tags in a baseline population.  If this is not present then an equal frequency in the baseline is assumed.
+This is practically independent of the poisson error if the 
+depth of coverage is high enough.  If the depth is lower then we
+can correct 
 
+### R Functions
+
+We estimate Nei's G using the function `estimate_G`.  This can take four parameters.
+
+* The first is the count in the tissueof interrest
+* The second (if present) gives the frequency of tags in a baseline population.  If this is not 
+present then an equal frequency in the baseline is assumed.
+* The third (called `min_count`) is a filter on the tags.  If any tag has a count of less
+than `min_count` in the baseline, then it is not used in the estimate.
+* The fourth (`min_p`) is another filter on the tags.  If any tag has a relative frequency of less
+than `min_p` in the baseline then it is not used in the estimates.
 
 ```{r}
 estimate_G(ex$Pop1_A)
 estimate_G(ex$Pop1_A, ex$Pop2_A, min_p=0.0001)
 estimate_G(ex$Pop2_C, ex$Pop1_A, min_p=0.0001)
+estimate_G(ex$Pop2_C, ex$Pop1_A, min_count=100)
 ```
-![plot of closest tags](fig/closest.png)
+
+#### Performance of Estimator
+
+We can test the performance of this estimator by comparing with simulated data.
+
+```{r}
+Estimated_G <- numeric(1000)
+for (i in 1:1000) {
+   aa <- simulate_drifttags(1000, 200000, drift_G=0.05)
+   Estimated_G[i] <- estimate_G(aa$count, aa$background_count)
+}
+require(ggplot2)
+qplot(Estimated_G, binwidth=0.01)
+```
 
